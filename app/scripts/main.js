fdd9834409a8d272bb15b17362dd5e9ad5358344
kitdemonio <ddd48533@gmail.com>
2016-04-28 14:22:16 +0200

    Update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function() {
+  $('.substruct').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 0 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  $('.add').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+  });
+});
